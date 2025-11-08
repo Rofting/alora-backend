0f6e3e8e585c3de68b8b4c9192a210a1ee08d445
Rofting <a27941@svalero.com>
2025-11-08 14:42:35 +0100

    Iniciamos la estructura inicial del proyecto

---
+++ src/main/java/com/alora/AloraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AloraBackendApplication.class, args);
+    }
+
+}
